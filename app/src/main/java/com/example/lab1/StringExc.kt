a7ad23bcc19ec816fb826f12a654df60379a5e2e
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-10-19 17:58:29 +0300

    Update StringExc.kt

--- app/src/main/java/com/example/lab1/StringExc.kt
+++ app/src/main/java/com/example/lab1/StringExc.kt
@@ -5,7 +5,6 @@ class StringExc{
     var maxSize = 0;
     var maxSentence: String = ""
     val text = "Mauris faucibus porttitor tincidunt. Sed orci libero, efficitur id eleifend sit amet, hendrerit quis nibh. Cras et commodo lectus. Maecenas condimentum semper condimentum non, mollis viverra massa. Donec cursus maximus gravida. Pellentesque vitae cursus magna, at egestas nulla. Ut eget augue eget magna mattis convallis a sed nisl. Donec ut porta lorem, et dapibus dolor. Pellentesque ac erat tortor."
-
     var sentences = text.split(".", "?", "!").filter{ it.isNotEmpty() }
 
     fun smt(): String{
